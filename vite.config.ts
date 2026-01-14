bcd438828081b4b3564e7f9b7e11e8adbf173112
João Ricardo <74267712+joaorbian@users.noreply.github.com>
2026-01-14 18:32:03 -0300

    asdsada

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/barbearia/', // 👈 IMPORTANTE para GitHub Pages
-
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
+  base: '/barbearia/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
